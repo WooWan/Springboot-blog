d1771ed800798c9d8aafe508648a10f0f039180f
wooho <woohobi@gmail.com>
2021-09-07 23:58:14 +0900

    비밀번호 db에 삽입할 때 암호화

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.cos.photogramstart.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super 삭제 - 기존 시큐리티가 가지고 있는 기능 비활성화
