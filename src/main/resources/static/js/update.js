77733631d88ba6ed816099d7d3737e84e8868f9f
wooho <woohobi@gmail.com>
2021-09-10 01:36:36 +0900

    회원정보 update 추가

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,4 +1,21 @@
 // (1) 회원정보 수정
-function update() {
-
+function update(userId, event) {
+    event.preventDefault();
+    let data =$("#profileUpdate").serialize();
+    console.log(data)
+    $.ajax({
+        type:  "put",
+        url: `/api/user/${userId}`,
+        data:data,
+        contentType: "application/x-www-form-urlencoded"
+    }).done(res=>{//HttpStatus 200번대
+        console.log("update 성공")
+        location.href =`/user/${userId}`
+    }).fail(error =>{// HTTPstatus 200을 제외한 상태코드
+        if (error.data == null) {
+            alert(error.responseJSON.message)
+        }else{
+            alert(JSON.stringify(error.responseJSON.data))
+        }
+    })
 }
