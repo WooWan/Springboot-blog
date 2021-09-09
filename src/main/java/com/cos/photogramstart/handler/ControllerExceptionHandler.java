5ed893152f707b6064719d070f32acff5c83502c
wooho <woohobi@gmail.com>
2021-09-10 01:32:17 +0900

    공통 예외 처리 추가

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.cos.photogramstart.handler;
 
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.auth.CMRespDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,16 @@ import java.util.Map;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    // jsp -> script return
     @ExceptionHandler(CustomValidationException.class)
     public String validationException(CustomValidationException e) {
         return Script.back(e.getErrorMap().toString());
+    }
+
+    //api 통신할때는 데이터 return
+    @ExceptionHandler(CustomValidationApiException.class)
+    public ResponseEntity<CMRespDto<?>> validationException(CustomValidationApiException e) {
+        return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()),
+                HttpStatus.BAD_REQUEST);
     }
 }
