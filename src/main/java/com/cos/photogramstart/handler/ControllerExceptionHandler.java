374fc235c3ef67a9564639e760568851c3970d5d
wooho <woohobi@gmail.com>
2021-09-13 01:53:17 +0900

    공통 응답, 에러 추가

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.handler;
 
+import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
@@ -19,13 +21,28 @@ public class ControllerExceptionHandler {
     // jsp -> script return
     @ExceptionHandler(CustomValidationException.class)
     public String validationException(CustomValidationException e) {
-        return Script.back(e.getErrorMap().toString());
+        if (e.getErrorMap() == null) {
+            return Script.back(e.getMessage());
+        }else{
+            return Script.back(e.getErrorMap().toString());
+        }
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public String Exception(CustomValidationException e) {
+        return Script.back(e.getMessage());
     }
 
     //api 통신할때는 데이터 return
     @ExceptionHandler(CustomValidationApiException.class)
     public ResponseEntity<CMRespDto<?>> validationException(CustomValidationApiException e) {
         return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<CMRespDto<?>> apiException(CustomApiException e) {
+        return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(),null),
                 HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/cos/photogramstart/web/dto/auth/CMRespDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/CMRespDto.java
@@ -1,10 +1,14 @@
 package com.cos.photogramstart.web.dto.auth;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class CMRespDto<T> {
 
     private int code;
