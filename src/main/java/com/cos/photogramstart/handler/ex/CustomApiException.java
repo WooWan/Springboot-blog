103e00c2f00f034c1ae0b5cf4dc1394d78eb8bd0
wooho <woohobi@gmail.com>
2021-09-16 02:08:38 +0900

    공통 에러 처리

--- src/main/java/com/cos/photogramstart/handler/ex/CustomApiException.java
+++ src/main/java/com/cos/photogramstart/handler/ex/CustomApiException.java
@@ -1,7 +1,5 @@
 package com.cos.photogramstart.handler.ex;
 
-import java.util.Map;
-
 public class CustomApiException extends RuntimeException{
 
     private static final long serialVersionUID = 1L;
