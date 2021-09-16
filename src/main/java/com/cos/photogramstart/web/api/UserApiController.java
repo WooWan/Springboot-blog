17b24f48dd91401f3f38c47124163afab2c1f5c0
wooho <woohobi@gmail.com>
2021-09-17 01:16:23 +0900

    aop error handling 추가

--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -1,9 +1,8 @@
 package com.cos.photogramstart.web.api;
 
 
-import com.cos.photogramstart.config.PrincipalDetails;
+import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.user.User;
-import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.service.SubscribeService;
 import com.cos.photogramstart.service.UserService;
 import com.cos.photogramstart.web.dto.auth.CMRespDto;
@@ -14,18 +13,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.mail.Multipart;
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -53,17 +48,8 @@ public class UserApiController {
                                @Valid UserUpdateDto userUpdateDto,
                                BindingResult bindingResult,// @Valid 다음 parameter에 적어놔야한다.
                                @AuthenticationPrincipal PrincipalDetails principalDetails) {
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error : bindingResult.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-                System.out.println(error.getDefaultMessage());
-            }
-            throw new CustomValidationApiException("유효성 검사 실패함", errorMap);
-        }else{
-            User userEntity = userService.update(id, userUpdateDto.toEntity());
-            principalDetails.setUser(userEntity);
-            return new CMRespDto<>(1, "회원수정완료", userEntity);
-        }
+        User userEntity = userService.update(id, userUpdateDto.toEntity());
+        principalDetails.setUser(userEntity);
+        return new CMRespDto<>(1, "회원수정완료", userEntity);
     }
 }
