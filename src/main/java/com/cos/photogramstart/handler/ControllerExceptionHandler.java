525d57791d8e4e8f7e081c4e3a9b1529d9465930
wooho <woohobi@gmail.com>
2021-09-17 01:17:19 +0900

    필요없는 import 삭제

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
